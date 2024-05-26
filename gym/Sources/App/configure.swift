860090c014d1e018806d1c7f169c217fbd3d3ede
Bruna <bruna.kastela@gmail.com>
2024-05-26 03:58:02 +0200

    deploy

--- gym/Sources/App/configure.swift
+++ gym/Sources/App/configure.swift
@@ -6,31 +6,34 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: "localhost",
-        port: 5432,
-        username: "postgres",
-        password: "postgres",
-        database: "gymdb",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        try app.databases.use(.postgres(url: databaseURL), as: .psql)
+    } else {
+        app.migrations.add(CreateUserType())
+        app.migrations.add(CreateSubscriptionType())
+        app.migrations.add(CreateUser())
+        app.migrations.add(CreateMembership())
+        app.migrations.add(CreateSubscription())
+        app.migrations.add(CreateLocation())
+        app.migrations.add(CreateTimeslot())
+        app.migrations.add(CreateTimeslotLocation())
+        app.migrations.add(CreateReservation())
 
-    app.migrations.add(CreateUserType())
-    app.migrations.add(CreateSubscriptionType())
-    app.migrations.add(CreateUser())
-    app.migrations.add(CreateMembership())
-    app.migrations.add(CreateSubscription())
-    app.migrations.add(CreateLocation())
-    app.migrations.add(CreateTimeslot())
-    app.migrations.add(CreateTimeslotLocation())
-    app.migrations.add(CreateReservation())
+        app.views.use(.leaf)
 
-    app.views.use(.leaf)
+        try await app.autoRevert()
+        try await app.autoMigrate()
 
-    try await app.autoRevert()
-    try await app.autoMigrate()
+        DatabaseSetup().setUpDatabase(using: app.db)
+    }
 
-    DatabaseSetup().setUpDatabase(using: app.db)
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    app.middleware.use(cors, at: .beginning)
 
     try routes(app)
 }
